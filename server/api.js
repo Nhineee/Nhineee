e842d123ace152be4e3c74ae2928c6ce342149e3
JEN-VO <134507327+JEN-VO@users.noreply.github.com>
2023-07-20 19:03:00 +0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -34,34 +34,80 @@ router.route('/register').post(async (request, response) => {
 
     await dboperations.register(request, response);
 });
-// *- GET -* //
+// *- GET TASK-* //
 router.route('/task').get(async (request, response) => {
     dboperations.getTask().then(result =>{
         response.status(200).json(result)
     })
 });
 
-// *- ADD -* //
+// *- ADD TASK-* //
 router.route('/task').post(async (request, response) => {
     let task = {...request.body}
     dboperations.addTask(task).then(result =>{
         response.status(200).json(result)
     })
 });
-// *- UPDATE -* //
+// *- UPDATE TASK-* //
 router.route('/task/:taskid').put(async (request, response) => {
     dboperations.updateTask(request.params.taskid,request.body).then(result =>{
         response.status(200).json(result)
     })
 });
 
-// *- DELETE -* //
+// *- DELETE TASK-* //
 router.route('/task/:taskid').delete(async (request, response) => {
     dboperations.delTask(request.params.taskid).then(result =>{
         response.status(200).json(result)
     })
 });
 
+// *- GET USER -* //
+router.route('/user').get(async (request, response) => {
+    dboperations.getUser().then(result =>{
+        response.status(200).json(result)
+    })
+});
+
+// *- UPDATE USER-* //
+router.route('/user/:id').put(async (request, response) => {
+    dboperations.updateUser(request.params.id,request.body).then(result =>{
+        response.status(200).json(result)
+    })
+});
+// *- DELETE USER -* //
+router.route('/user/:id').delete(async (request, response) => {
+    dboperations.delUser(request.params.id).then(result =>{
+        response.status(200).json(result)
+    })
+});
+// *- GET COM -* //
+router.route('/com').get(async (request, response) => {
+    dboperations.getTask().then(result =>{
+        response.status(200).json(result)
+    })
+});
+
+// *- ADD COM-* //
+router.route('/com').post(async (request, response) => {
+    let com = {...request.body}
+    dboperations.addCom(com).then(result =>{
+        response.status(200).json(result)
+    })
+});
+// *- UPDATE COM-* //
+router.route('/com/:comid').put(async (request, response) => {
+    dboperations.updateCom(request.params.comid,request.body).then(result =>{
+        response.status(200).json(result)
+    })
+});
+
+// *- DELETE COM-* //
+router.route('/com/:comid').delete(async (request, response) => {
+    dboperations.delCom(request.params.comid).then(result =>{
+        response.status(200).json(result)
+    })
+});
 // *- TEST -* //
 router.route('/test').get(async (request, response) => {
     await dboperations.test().then(result => {
